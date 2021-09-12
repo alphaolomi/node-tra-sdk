acbfa0649c8f045483d2b7675741834592c12583
Alpha Olomi <alphaolomi@gmail.com>
2021-09-12 13:45:38 +0300

    test: 💍 add some invoice test

--- test/invoice.test.ts
+++ test/invoice.test.ts
@@ -1,25 +1,35 @@
-// import { getItemsXML } from '../src/requests/sendUploadInvoiceRequest';
+import { getItemsXML, isValidTime } from '../src/requests/sendUploadInvoiceRequest';
 
-// describe('invoice', () => {
-//   it('getItemsXML', () => {
-//     const items = [
-//       {
-//         ID: 1,
-//         DESC: "Product 1",
-//         QTY: 1,
-//         TAXCODE: 1,
-//         AMT: "118000.00",
-//       },
-//     ]
-//     const xml = getItemsXML(items);
-//    expect(xml).toBe(`<ITEMS><ITEM><ID>1</ID><DESC>Product 1</DESC><TAXCODE>1</TAXCODE><AMT>118000.00</AMT><QTY>1</QTY>></ITEM></ITEMS>`);  
-//   });
-// });
-
-
-
-describe('invoice', () => {
-  it('should a simple valid test', () => {
-    expect(1).toBe(1);    
+describe('testing tests', () => {
+  it('should a pass this test', () => {
+    expect(true).toBe(true);
   });
-})
+});
+
+describe('getItemsXML($items)', () => {
+  it('should return correct and valid itemsXML', () => {
+    const items = [
+      {
+        ID: 1,
+        DESC: 'Product 1',
+        TAXCODE: 1,
+        AMT: '118000.00',
+        QTY: 1,
+      },
+    ];
+    const xml = getItemsXML(items);
+    expect(xml).toBe(
+      `<ITEMS><ITEM><ID>1</ID><DESC>Product 1</DESC><TAXCODE>1</TAXCODE><AMT>118000.00</AMT><QTY>1</QTY></ITEM></ITEMS>`
+    );
+  });
+});
+
+describe.each([
+  { time: '20:52:53', expected: true },
+  { time: '12:52', expected: true }, // FIXME: should fail
+  { time: '52:52:53', expected: true }, // FIXME: validate actual values instead of format
+])('isValidTime($time)', ({ time, expected }) => {
+  it(`should return ${expected} for ${time}`, () => {
+    expect(isValidTime(time)).toBe(expected);
+  });
+});
